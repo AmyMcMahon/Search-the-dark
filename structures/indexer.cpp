3e36bfd29cdef92f6ed776519ae8d53e4097124d
AmyMcMahon <admcmahon@live.com[3~[3~>
2024-10-20 19:23:08 +0100

    praying so hard rn

--- structures/indexer.cpp
+++ structures/indexer.cpp
@@ -216,12 +216,14 @@ int main()
     Mappy index;
     std::string path = "../books";
 
-    //     // Iterate through files in the specified directory and add them to the index
-    //     for (const auto &entry : fs::directory_iterator(path))
-    //     {
-    //         idx.addFiley(entry.path().string(), index);
-    //     }
+    // Iterate through files in the specified directory and add them to the index
+    for (const auto &entry : fs::directory_iterator(path))
+    {
+        idx.addFiley(entry.path().string(), index);
+    }
 
-    //     // Export the index to CSV files
-    //     idx.toCsv(index);
-    // };
+    // Export the index to CSV files
+    idx.toCsv(index);
+
+    return 0;
+}
